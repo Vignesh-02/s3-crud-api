77cb6ba2740ce39a2a314b425f65121b546a15f0
Vignesh <vignesh@turnkey.work>
2026-01-09 14:31:29 +0530

    uncommented s3 imports

--- src/get.js
+++ src/get.js
@@ -1,7 +1,7 @@
-// const AWS = require('aws-sdk');
-// const s3 = new AWS.S3();
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
-// const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
 // module.exports.handler = async(event) => {
 //     console.log(event);
@@ -60,7 +60,7 @@ module.exports.handler = async (event) => {
 
     console.log('image data', data)
 
-    
+
     return {
       statusCode: 200,
       isBase64Encoded: true, // ✅ REQUIRED
